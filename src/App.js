6faf000b6362dd43fa201deafb86a1b154124636
Lavender-Irons <lavenderirons@gmail.com>
2020-12-29 09:36:33 -0600

    added navbar and social icons

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import Contact from "./components/Contact";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import SingleProject from './components/SingleProject';
+import Blog from './components/Blog';
+import SingleBlog from './components/SingleBlog';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <BrowserRouter>
+    <NavBar />
        <Switch>
          <Route component={Home} path='/' exact/>
          <Route component={About} path='/about' />
          <Route component={Contact} path='/contact' />
          <Route component={SingleProject} path='/projects/:slug'/>
          <Route component={Projects} path='/projects' />
+         <Route component={SingleBlog} path='/blog/:slug'/>
+         <Route component={Blog} path='/blog'/>
        </Switch>
     </BrowserRouter>
   )
---
+++ src/components/NavBar.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
+
+export default function NavBar(){
+    return (
+        <header className="bg-purple-900">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+
+                    <NavLink 
+                    to='/' 
+                    exact 
+                    activeClassName="text-indigo-300"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-4xl font-bold cursive tracking-widest">
+                    Home
+                    </NavLink>
+
+                    <NavLink 
+                    to='/about'
+                    activeClassName="text-indigo-300" 
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-3xl font-bold cursive">
+                    About
+                    </NavLink>
+
+                    <NavLink 
+                    to='/projects' 
+                    activeClassName="text-indigo-300"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-3xl font-bold cursive">
+                    Projects
+                    </NavLink>
+
+                    <NavLink 
+                    to='/blog' 
+                    activeClassName="text-indigo-300"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-3xl font-bold cursive">
+                    Blog
+                    </NavLink>
+
+                    <NavLink 
+                    to='/contact'
+                    activeClassName="text-indigo-300"
+                    className="inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-3xl font-bold cursive">
+                    Contact
+                    </NavLink>
+
+                </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon 
+                    url="https://www.linkedin.com/in/lavender-irons-908828117/" 
+                    className="mr-4" 
+                    target="_blank" 
+                    fgColor="#fff" 
+                    style={{ height: 35, width: 35 }}/>
+
+                    <SocialIcon 
+                    url="https://github.com/Lavender-Irons" 
+                    className="mr-4" 
+                    target="_blank" 
+                    fgColor="#fff" 
+                    style={{ height: 35, width: 35 }}/>
+                </div>
+            </div>
+        </header>
+    )
+}
